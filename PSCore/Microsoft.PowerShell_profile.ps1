a10ee6a9ecafb047e704ddd808d809edd475a578
David Woods <dwoo@microsoft.com>
2019-09-20 14:47:37 -0700

    Adding PSCore profile, with different .exe for elevation

---
+++ PSCore/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+﻿. "$env:HOMEPATH\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+function elevate {
+    $here = Get-Location
+    $args = ("-NoExit","-Command `"pushd '$here'`"")
+    Start-Process pwsh.exe -Verb RunAs -ArgumentList $args
+}
