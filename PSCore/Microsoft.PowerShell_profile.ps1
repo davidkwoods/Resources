8605dceb968804b200f4ca72c141204bd50e6fbb
David Woods <dwoo@microsoft.com>
2019-09-30 17:10:59 -0700

    Fixing PSCore profile path to regular PS profile

--- PSCore/Microsoft.PowerShell_profile.ps1
+++ PSCore/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-﻿. "$env:HOMEPATH\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+﻿. "~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 function elevate {
     $here = Get-Location
     $args = ("-NoExit","-Command `"pushd '$here'`"")
