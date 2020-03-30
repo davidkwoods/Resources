185258a62390edc74a7857d9fa45d7fd1908f713
David Woods <dwoo@microsoft.com>
2020-03-30 10:12:21 -0700

    Adding link to upstream oh-my-posh in comment

--- PSCore/Microsoft.PowerShell_profile.ps1
+++ PSCore/Microsoft.PowerShell_profile.ps1
@@ -6,6 +6,6 @@ function elevate {
     Start-Process pwsh.exe -Verb RunAs -ArgumentList $args
 }
 
-# From https://github.com/davidkwoods/oh-my-posh.git
+# From https://github.com/davidkwoods/oh-my-posh.git  (Fork of https://github.com/JanDeDobbeleer/oh-my-posh.git)
 Import-Module "C:\git\oh-my-posh\oh-my-posh.psd1"
 Set-Theme PsCoreSeparated
